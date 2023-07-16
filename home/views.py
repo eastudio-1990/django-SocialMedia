c395933e9274a172e08639bd9847ec87328a22d7
esmaeil <eastudio1990@gmail.com>
2023-07-16 20:20:06 +0330

    update Post Added

--- home/views.py
+++ home/views.py
@@ -4,6 +4,7 @@ from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.shortcuts import redirect
+from .forms import PostUpdateForm
 class HomeView(View):
     def get(self,request):
         posts = Post.objects.all()
@@ -22,5 +23,22 @@ class PostDeleteView(LoginRequiredMixin,View):
             post.delete()
             messages.success(request,'Post Deleted','success')
         else:
-            messages.error(request,'You Cant Delete This Post')
+            messages.error(request,'You Cant Delete This Post','danger')
         return redirect('home:home')
+
+class PostUpdateView(LoginRequiredMixin,View):
+    form_class = PostUpdateForm
+
+    def dispatch(self,request,*args,**kwargs):
+        post = Post.objects.get(pk=kwargs['post_id'])
+        if not post.user.id == request.user.id:
+            messages.error(request, 'You Cant Update This Post', 'danger')
+            return redirect('home:home')
+        return  super().dispatch(request,*args,**kwargs)
+
+    def get(self, request, post_id):
+        post = Post.objects.get(pk=post_id)
+        form = self.form_class(instance=post)
+        return  render(request,'home/update.html',{'form':form})
+    def post(self,request, post_id):
+       pass
