2af23d982badc0365a16cd2b4d71fdcdfc570e6a
esmaeil <eastudio1990@gmail.com>
2023-07-16 14:39:56 +0330

    command deleted

--- home/admin.py
+++ home/admin.py
@@ -3,5 +3,13 @@ from .models import Post
 
 
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('user','slug','updated')
+    search_fields = ('slug','body')
+    list_filter = ('updated',)
+    prepopulated_fields = {'slug': ('body',)}
+    raw_id_fields = ('user',)
+
+
 
