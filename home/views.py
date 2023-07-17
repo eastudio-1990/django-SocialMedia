22ed7198e4933701abf8e204d674338bfd580a1e
esmaeil <eastudio1990@gmail.com>
2023-07-17 09:24:58 +0330

    404 Page added

--- home/views.py
+++ home/views.py
@@ -3,7 +3,7 @@ from django.views import View
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
-from django.shortcuts import redirect
+from django.shortcuts import redirect,render,get_object_or_404
 from .forms import PostCreateUpdateForm
 from django.utils.text import slugify
 
@@ -16,12 +16,12 @@ class HomeView(View):
 
 class PostDetailView(View):
     def get(self,request,post_id,post_slug):
-        post = Post.objects.get(id=post_id,slug=post_slug)
+        post = get_object_or_404(Post, pk=post_id,slug=post_slug)
         return render(request,'home/detail.html', {'post':post})
 
 class PostDeleteView(LoginRequiredMixin,View):
     def get(self,request,post_id):
-        post= Post.objects.get(pk=post_id)
+        post= get_object_or_404(Post,pk=post_id)
         if post.user.id == request.user.id:
             post.delete()
             messages.success(request,'Post Deleted','success')
@@ -33,7 +33,7 @@ class PostUpdateView(LoginRequiredMixin,View):
     form_class = PostCreateUpdateForm
 
     def setup(self, request, *args, **kwargs):
-        self.post_instance = Post.objects.get(pk=kwargs['post_id'])
+        self.post_instance = get_object_or_404(Post, pk=kwargs['post_id'])
         return super().setup(request,*args,**kwargs)
     def dispatch(self,request,*args,**kwargs):
         post = self.post_instance
