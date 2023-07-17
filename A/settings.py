c32ab500e81565970cee9cb9340e3f0bc3a74898
esmaeil <eastudio1990@gmail.com>
2023-07-17 09:50:44 +0330

    smtp added

--- A/settings.py
+++ A/settings.py
@@ -130,3 +130,13 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 LOGIN_URL = '/account/login/'
+
+
+# Google Account
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackEnd'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'eastudio1990@gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_PASSWORD = 'nlacnmblomwxoiae'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'Esmaeil_Ashrafi'
